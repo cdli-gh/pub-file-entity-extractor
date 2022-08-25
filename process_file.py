a643ca2079845f0f72b534493381d421fe94869e
Circle Chen <circlecly@berkeley.edu>
2022-08-24 22:11:14 -0700

    Better abstraction

--- process_file.py
+++ process_file.py
@@ -1,58 +1,56 @@
 import pdfplumber
-import argparse
 import pandas as pd
 import re
-import sqlalchemy
 
-db_server = "mariadb+mariadbconnector://root@127.0.0.1:23306/cdli_db"
+class PubEntityExtractor():
 
-def get_text(pdf, separator = " "):
-    full_txt = ""
-    for page in pdf.pages:
-        full_txt += page.extract_text(x_tolerance = 1) + separator
-    return full_txt
+    db_server = "mariadb+mariadbconnector://root@127.0.0.1:23306/cdli_db"
 
-def is_image_pdf(pdf):
-    return len(get_text(pdf, "")) == 0
+    def get_text(self, pdf, separator = " "):
+        full_txt = ""
+        for page in pdf.pages:
+            full_txt += page.extract_text(x_tolerance = 1) + separator
+        return full_txt
 
-def get_all_text(pdf):
-    return get_text(pdf, " ")
+    def is_image_pdf(self, pdf):
+        return len(self.get_text(pdf, "")) == 0
 
-def get_provenience_occurences(full_txt):
-    engine = sqlalchemy.create_engine(db_server)
-    connection = engine.connect()
-    proveniences = pd.read_sql('SELECT id, provenience FROM proveniences', connection, index_col = "id")
-    prov_names = proveniences["provenience"].str.findall(r"(.+) \(mod\. (.+)\)").str[0]
-    proveniences["ancient_name"] = prov_names.str[0]
-    proveniences["modern_name"] = prov_names.str[1]
-    proveniences = proveniences.drop(207)
-    proveniences.loc[242, "ancient_name"] = "Qattara"
-    proveniences.loc[242, "modern_name"] = "Tell al Rimah"
-    proveniences.loc[360, "ancient_name"] = "Kian"
-    proveniences.loc[360, "modern_name"] = "Tell Shmid"
+    def get_all_text(self, pdf):
+        return self.get_text(pdf, " ")
 
-    def find_total_occurence(row):
-        ancient_name = row["ancient_name"]
-        modern_name = row["modern_name"]
-        pattern_a = r'\b' + ancient_name + r'\b'
-        occur_a = 0 if ancient_name == "uncertain" else len(re.findall(pattern_a, full_txt))
-        pattern_b = r'\b' + modern_name + r'\b'
-        occur_b = 0 if modern_name == "uncertain" else len(re.findall(pattern_b, full_txt))
-        return occur_a + occur_b
+    def get_provenience_occurences(self, full_txt, prov_location = "proveniences.csv"):
+        # engine = sqlalchemy.create_engine(self.db_server)
+        # connection = engine.connect()
+        # proveniences = pd.read_sql('SELECT id, provenience FROM proveniences', connection, index_col = "id")
+        proveniences = pd.read_csv(prov_location, index_col = "id")
+        prov_names = proveniences["provenience"].str.findall(r"(.+) \(mod\. (.+)\)").str[0]
+        proveniences["ancient_name"] = prov_names.str[0]
+        proveniences["modern_name"] = prov_names.str[1]
+        proveniences = proveniences.drop(207)
+        proveniences.loc[242, "ancient_name"] = "Qattara"
+        proveniences.loc[242, "modern_name"] = "Tell al Rimah"
+        proveniences.loc[360, "ancient_name"] = "Kian"
+        proveniences.loc[360, "modern_name"] = "Tell Shmid"
 
-    proveniences["total_occurences"] = proveniences.apply(find_total_occurence, axis = "columns")
-    proveniences = proveniences.sort_values("total_occurences", ascending = False)
-    return proveniences[proveniences["total_occurences"] > 0]
+        def find_total_occurence(row):
+            ancient_name = row["ancient_name"]
+            modern_name = row["modern_name"]
+            pattern_a = r'\b' + ancient_name + r'\b'
+            occur_a = 0 if ancient_name == "uncertain" else len(re.findall(pattern_a, full_txt))
+            pattern_b = r'\b' + modern_name + r'\b'
+            occur_b = 0 if modern_name == "uncertain" else len(re.findall(pattern_b, full_txt))
+            return occur_a + occur_b
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Publication file processor.')
-    parser.add_argument('-n', '--file_name', type=str, help='the input publication filename')
+        proveniences["total_occurences"] = proveniences.apply(find_total_occurence, axis = "columns")
+        proveniences = proveniences.sort_values("total_occurences", ascending = False)
+        return proveniences[proveniences["total_occurences"] > 0]
 
-    args = parser.parse_args()
-    with pdfplumber.open(args.file_name) as pdf:
-        if is_image_pdf(pdf):
-            # TODO: Perform OCR on pdf.
-            pass
-        else:
-            text = get_all_text(pdf)
-            print(get_provenience_occurences(text))
+    def parse(self, file_name, prov_location = "proveniences.csv"):
+        with pdfplumber.open(file_name) as pdf:
+            if self.is_image_pdf(pdf):
+                # TODO: Perform OCR on pdf.
+                pass
+            else:
+                text = self.get_all_text(pdf)
+                print(self.get_provenience_occurences(text, prov_location))
+
